278e48ceeeb962a2d76f50d1bff769c3b22d61cb
KyoheiYamamoto <arienkyohei@gmail.com>
2020-05-15 20:04:51 +0900

    ビューとレイアウトテンプレート作成完了

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>memoapp</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+</head>
+    <body>
+        <header class="nav navbar-light bg-light">
+            <div class="container">
+                <a class="navbar-brand" href="">Hacksレビュー</a>
+            </div>
+        </header>
+
+        <main>
+            @yield('content')
+        </main>
+        <footer class="footer bg-dark">
+            <div class="container">
+                <p class="text-white text-right">@kyohei-yamamoto All right reserved</p>
+            </div>
+        </footer>
+    </body>
+</html>
---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,7 @@
+@extends('layout')
+
+@section('content')
+    <div>
+        aaaaa
+    </div>
+@endsection('content')
